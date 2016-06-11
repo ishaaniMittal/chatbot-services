88b5df9c7ca7590080f64056c1a50ba8151e6168
imittal <ishaani.94@gmail.com>
2016-06-11 11:14:03 +0530

    first commit

---
+++ src/main/java/com/valarmorghulis/ChatbotServicesApplication.java
@@ -0,0 +1,12 @@
+package com.valarmorghulis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotServicesApplication.class, args);
+	}
+}
