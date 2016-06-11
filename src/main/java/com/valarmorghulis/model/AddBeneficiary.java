5bd4c43f89bbf14ed81ae7b821d61cf4fe4bebba
imittal <ishaani.94@gmail.com>
2016-06-11 23:51:31 +0530

    Add Beneficiary Service

--- src/main/java/com/valarmorghulis/model/AddBeneficiary.java
+++ src/main/java/com/valarmorghulis/model/AddBeneficiary.java
@@ -3,7 +3,7 @@ package com.valarmorghulis.model;
 /**
  * Created by imittal on 6/11/16.
  */
-public class AddBeneficiaryRequest {
+public class AddBeneficiary {
     private String name;
     private String ifscCode;
     private String accountNumber;
