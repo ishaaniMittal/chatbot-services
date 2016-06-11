8d0d14725d3058c4d8f84e7f33b6538b683d4edb
imittal <ishaani.94@gmail.com>
2016-06-11 23:49:48 +0530

    Bot Response

---
+++ src/main/java/com/valarmorghulis/model/BotResp.java
@@ -0,0 +1,22 @@
+package com.valarmorghulis.model;
+
+/**
+ * Created by imittal on 6/11/16.
+ */
+public class BotResp {
+
+    private String response;
+
+    public BotResp(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+}
